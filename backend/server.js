f462211cd2b9714bd89f4c2eff1ba5bb18e4daec
Jazz-21 <tiwaryjoya@gmail.com>
2022-06-02 15:21:59 +0200

    fixing error

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ import { fileURLToPath } from 'url'
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
+const distPath = path.join(__dirname, '/../dist/')
 
 const app = express();
 import hamsters from './routes/hamsters.js'
@@ -23,12 +24,16 @@ app.use( express.json() );
 app.use( cors() );
 //app.use( express.static(staticFolder) );
 app.use('/HamPics', express.static(picsFolder) );
+app.use(express.static(distPath));
 
 
 
 // api for hamsters
 app.use('/hamsters', hamsters);
 
+app.all('*', (req, res) => {
+	res.sendFile(distPath + 'index.html')
+  })
 
 //Starting server
 app.listen(PORT, () => {
